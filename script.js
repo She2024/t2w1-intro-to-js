396aa0b86442993c25f010a64aa52257d21617ec
she2024 <bullybluedog@hotmail.com>
2024-10-15 20:03:26 +1000

    added intro to JS

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Hello World")
+
+// x = 5; // global variable can be accessed anywhere in the program difficult to trak not recommended
+
+// var x = 5; // limited to specific function it's entered in not recommended
+
+let x = 5; // preffered for values with changeable values can be 
+
+const x = 5; //prefered use everywhere the value does not need to be changed. 
